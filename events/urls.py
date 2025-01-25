69f151d5b0f537c062ca5170572acd3c0bad07d8
Aporna16 <rakshit.aporna@gmail.com>
2025-01-26 03:22:52 +0600

    Added dashboard, tailwind from module-4

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from events.views import show_event
+from events.views import manager_dashboard, user_dashboard, test
 
 urlpatterns = [
-    path('show-event/', show_event)
+    path('manager-dashboard/', manager_dashboard),
+    path('user-dashboard/', user_dashboard),
+    path('test/',test)
 ]
--- events/views.py
+++ events/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Welcome to the Event management system")
 
-def contact(request):
-    return HttpResponse("<h1>This is contact</h1>")
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
 
-def show_event(reqruest):
-    return HttpResponse("This is our task page")
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
+
+def test(request):
+    return render(request, 'test.html')
