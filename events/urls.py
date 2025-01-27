66f124bf15f50817c32ee79d01fac875edca994b
Aporna16 <rakshit.aporna@gmail.com>
2025-01-28 03:06:29 +0600

    Forms and ModelForm

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from events.views import manager_dashboard, user_dashboard, test
+from events.views import manager_dashboard, user_dashboard, test, create_event
 
 urlpatterns = [
     path('manager-dashboard/', manager_dashboard),
     path('user-dashboard/', user_dashboard),
-    path('test/',test)
+    path('test/',test),
+    path('create-event/',create_event)
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from events.forms import EventForm,EventModelForm
+from events.models import Employee,Event
 
 # Create your views here.
 
@@ -21,3 +23,33 @@ def test(request):
         "count" : count
     }
     return render(request, 'test.html',context)
+
+def create_event(request):
+    # employees = Employee.objects.all()
+    form = EventModelForm()
+
+    if request.method == "POST":
+        form = EventModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return render(request, 'event_form.html',{"form":form, "message": "Event added successfully"})
+
+            """ For Model form data """
+            ''' For django data'''
+            # data = form.cleaned_data
+            # title = data.get('title')
+            # description = data.get('description')
+            # due_date = data.get('due_date')
+            # assigned_to = data.get('assigned_to')
+
+            # event = Event.objects.create(
+            #     title=title, description=description,due_date=due_date)
+            # for emp_id in assigned_to:
+            #     employee = Employee.objects.get(id=emp_id)
+            #     event.assigned_to.add(employee)
+
+            # return HttpResponse("Event Added successfully")
+
+    context = {"form": form}
+    return render(request,"event_form.html", context)
