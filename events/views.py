58a5df84c423316a89ecf48412dda758ff4d310c
Aporna16 <rakshit.aporna@gmail.com>
2025-01-31 01:59:25 +0600

    Form part added

--- events/views.py
+++ events/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render
-from events.forms import EventForm
-from events.models import Participant,Category
 from django.http import HttpResponse
+from events.forms import EventModelForm
+from events.models import Participant, Category
 
 # Create your views here.
 
 def manager_dashboard(request):
     return render(request, "dashboard/manager-dashboard.html")
 
-
 def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
 
 def create_event(request):
-    participants = Participant.objects.all()
-    categories = Category.objects.all()
-    form = EventForm(participants = participants, categories = categories)
+    form = EventModelForm()
+
+    if request.method == "POST":
+        form = EventModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return render(request, "event_form.html", {"form": form, "message": "Event added successfully"})
+
     context = {"form": form}
     return render(request, "event_form.html", context)
