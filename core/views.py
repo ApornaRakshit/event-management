2a2a55f1cd60834793d790b44cca613eab6cd394
ApornaRakshit <rakshit.aporna@gmail.com>
2025-08-06 00:43:06 +0600

    views.py

--- core/views.py
+++ core/views.py
@@ -2,11 +2,7 @@
 from django.shortcuts import render
 
 def home(request):
-    is_organizer = False
-    if request.user.is_authenticated:
-        is_organizer = request.user.groups.filter(name='Organizer').exists()
-    return render(request, 'home.html', {'is_organizer': is_organizer})
-
+   return render(request, 'home.html')
 
 def no_permission(request):
     return render(request, 'no_permission.html')
